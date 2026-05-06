33f976cdab2ff7c6be2da89339f2daa92e1dbd30
𝐀𝐝𝐦𝐢𝐧𝐢𝐬𝐭𝐫𝐚𝐭𝐨𝐫 <advinistrator@gmail.com>
2026-05-06 10:50:13 +0300

    Update namespace and audio source URL in BLS_alert.js

--- BLS_alert.js
+++ BLS_alert.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         BLS alert
-// @namespace    http://tampermonkey.net/
+// @namespace    http://blsspainmorocco.com/
 // @version      2.0.3
 // @description  bls aler  P|R|C Clean Refresh Alarm Notif
-// @author       @Itsmaarouf
+// @author       @unknown
 // @match        *://*.blsspainmorocco.com/*
 // @grant        none
 // ==/UserScript==
 
 var gun=setInterval(function(){
     if (document.getElementsByClassName('day activeClass').length !==0) {
-      new Audio('https://www.soundjay.com/mechanical/sounds/smoke-detector-1.mp3').play();
+      new Audio('https://www.blsspainmorocco.com/mechanical/sounds/smoke-detector-1.mp3').play();
       clearInterval(gun);
     }
 }, 1000)
